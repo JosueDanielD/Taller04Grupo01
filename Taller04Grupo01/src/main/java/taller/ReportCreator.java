6677a2587078030c8c8d982742f13c5f61a4522b
cimontesm <cimontes@espol.edu.ec>
2024-11-06 18:47:09 -0500

    Agregando clases

---
+++ Taller04Grupo01/src/main/java/taller/ReportCreator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller;
+
+/**
+ *
+ * @author CMONTES
+ */
+public class ReportCreator {
+
+    Report r;
+
+    public Report createReport() {
+
+        return r;
+    }
+
+    public void generateReport(String content, StyleOptions styleOptions) {
+
+    }
+}
---
+++ Taller04Grupo01/src/main/java/taller/WordReport.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller;
+
+/**
+ *
+ * @author CMONTES
+ */
+public class WordReport implements Report{
+
+    @Override
+    public void addContent(String content) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void applyStyles(StyleOptions styleOptions) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateContent() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
