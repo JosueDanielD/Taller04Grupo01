853fd0bdbc05cce8042be9e1dffc02374cac8915
cimontesm <cimontes@espol.edu.ec>
2024-11-06 18:05:48 -0500

    Creacion del taller

---
+++ Taller04Grupo01/src/main/java/taller/Taller04Grupo01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package taller;
+
+/**
+ *
+ * @author CMONTES
+ */
+public class Taller04Grupo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
